7abf884b41d769f107c8669cca28f615d593d71a
Olga Mokhova <omokhova@planner5d.com>
2020-02-16 17:46:47 +0300

    create pwa

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React, { Component}  from "react";
 import ReactDOM from "react-dom";
+import Logo from './img/logo.svg';
+import './style/main.scss';
 
 class HelloMessage extends React.Component {
     render() {
         return (
-            <div>
-                Hello, {this.props.name}
+            <div className='greetings'>
+                <img src={Logo} width={291} height={109}/>
+                <h3>Хей гайз 🤚 </h3>
+                <h3>Чтобы войти, введи пароль</h3>
             </div>
         );
     }
 }
 
 ReactDOM.render(
-    <HelloMessage name="Angel Cakes" />,
+    <HelloMessage name="Angel Cakes"/>,
     document.getElementById('root')
 );
