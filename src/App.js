3aa4714939a82dab4681e6469e689eb940a291e9
Olga Mokhova <omokhova@planner5d.com>
2020-02-16 13:33:28 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component}  from "react";
+import ReactDOM from "react-dom";
+
+class HelloMessage extends React.Component {
+    render() {
+        return (
+            <div>
+                Hello, {this.props.name}
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <HelloMessage name="Angel Cakes" />,
+    document.getElementById('root')
+);
